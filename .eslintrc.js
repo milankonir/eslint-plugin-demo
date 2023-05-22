ddf4da808a5bf205fadeafc7464474910ed97109
milankonir <milan.konir@outreach.io>
2023-05-22 10:34:25 +0200

    debug setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: ['@eslint-plugin-demo'],
   extends: ['plugin:@eslint-plugin-demo/recommended'],
   rules: {
-    '@eslint-plugin-demo/01-no-test-prefixed-function-names': 'off',
+    // '@eslint-plugin-demo/01-no-test-prefixed-function-names': 'off',
     '@eslint-plugin-demo/02-mandatory-comments': 'off',
     '@eslint-plugin-demo/03-theme-colors-only': 'off',
     '@eslint-plugin-demo/04-no-disabled-tests-without-todo': 'off',
