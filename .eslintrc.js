5172bba9c98ad1db6ae46407ff332dcbfa825291
milankonir <milan.konir@outreach.io>
2023-05-22 22:33:53 +0200

    add timing script

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     '@eslint-plugin-demo/01-no-test-prefixed-function-names': 'off',
     '@eslint-plugin-demo/02-mandatory-comments': 'off',
     '@eslint-plugin-demo/03-theme-colors-only': 'off',
-    '@eslint-plugin-demo/04-no-disabled-tests-without-todo': 'off',
+    '@eslint-plugin-demo/04-no-disabled-tests-without-todos': 'off',
     '@eslint-plugin-demo/05-todo-comments-format': 'off',
     '@eslint-plugin-demo/06-package-organization': 'off',
     '@eslint-plugin-demo/07-type-checking': 'off',
@@ -19,7 +19,7 @@ module.exports = {
       files: ['packages/06-package-organization/**'],
       rules: {
         '@eslint-plugin-demo/06-package-organization': [
-          'error',
+          'off', // 'error',
           {
             layers: [
               { name: 'components', index: 3 },
